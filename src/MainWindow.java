cb03834ec87a9fe569af17e72694d108492ca7b1
Vladilshk <vova.ilyshenko2016@gmail.com>
2022-07-01 15:18:01 +0300

    Initial commit

---
+++ src/MainWindow.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow(){
+        setTitle("Snake");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(368, 384);
+        setLocation(500, 200);
+        add(new GameField());
+        setVisible(true);
+    }
+
+
+
+    public static void main(String[] args){
+        MainWindow mw = new MainWindow();
+    }
+}
